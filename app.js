529fd9f2648b07f8dccbdccca416cf51193e5f77
858683682@qq.com <858683682@qq.com>
2015-11-06 17:14:09 +0800

    修改路由配置

--- app.js
+++ app.js
@@ -3,21 +3,16 @@ var app = express();
 
 app.use(express.static(__dirname+"/"));
 
-app.get('/', function(req, res){
+app.get([
+  '/',
+  '/about.html',
+  '/intro.html',
+  '/empoly.html'
+], function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/about.html', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-app.get('/intro.html', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
 
-app.get('/empoly.html', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.listen(3030, function(){
+app.listen(1999, function(){
     console.log('listening on *:3030');
 });
--- js/index.js
+++ js/index.js
@@ -1,21 +1,59 @@
-$('#J_tab').on('click','a',function(){
+var tab = $('#J_tab'),
+    allLinks = tab.find('a'),
+    main = $('main'),
+    pages = {
+      index: '<article><h2>首页</h2><section class="content-wrap">万恶我为首</section></article>',
+      about: '<article><h2>关于我们</h2><section class="content-wrap">我们是一群可爱的蓝精灵</section></article>',
+      intro: '<article><h2>产品介绍</h2><section class="content-wrap">我们不生产代码，只做代码的搬用工</section></article>',
+      empoly: '<article><h2>职位招聘</h2><section class="content-wrap">诚聘女友3名，年龄16-30岁，身高160-170，肤白貌美</section></article>'
+    },
+    timer = null;
+
+
+callback();
+
+// 点击侧栏
+tab.on('click','a',function(){
+  clearTimeout(timer);
+
+
   var href = $(this).attr('href');
-  window.history.pushState({page:href},'test',href);
-  callback();
+
+  timer = setTimeout(function(){
+    window.history.pushState({page:href},'test',href);
+    callback();
+  },200);
+
   return false;
 });
 
-
- var c = $('#J_content');
-
- callback();
-
- function callback(){
-   c.html(location.pathname);
- }
-
+// 监听浏览器pushState事件
  if('pushState' in history){
    window.addEventListener("popstate", function() {
-       callback();
+    callback();
    });
+ }
+
+ function callback(){
+   var pathname = location.pathname.match(/\/(.*)\.html/);
+   var html = pathname ? pathname[1] : "index";
+   var page = $(pages[html]);
+  var activePage = main.find('.page-active');
+
+   allLinks.removeClass('active');
+   $('#J_'+html).addClass('active');
+
+   main.append(page);
+
+   activePage.addClass('page-out');
+   setTimeout(function(){
+     activePage.remove();
+   },300);
+
+   setTimeout(function(){
+     page.addClass('page-active');
+   },10);
+
+
+
  }
